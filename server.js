0489ced9b816afb5df66b83823451444092837e2
Tim <FPcond@gmail.com>
2014-11-20 19:45:54 -0500

    hooked in firebase

--- server.js
+++ server.js
@@ -4,14 +4,59 @@
 var express = require("express");
 var http = require("http");
 var Twit = require("twit");
+var Firebase = require("firebase");
 var T = new Twit({
     consumer_key:         'tQBgTC7WSC2YKDYkdQgDCPVtx'
     , consumer_secret:      'xpMbSpmSy6d4HpKT0VFch70YNqBhTydDSXbrfW3137KJw9iI4C'
     , access_token:         '1374765799-O40L3jHmUeEVZbSpzaLiLLxyGlQgRPbSgSLtaVX'
     , access_token_secret:  'OYwgOSuqLpfiOtta5n3EpVP4u4debCClsfEEaWgNwqqGh'
 });
+var BPM = "bpm_playlist";
+var myFirebaseRef = new Firebase("https://groovebmp.firebaseio.com/" + BPM);
 
+function TinySong (A, S){
+    //var TinyKey = "f6834955de245c39810cf059ce77da5d";
+    A = A.replace(/ /g,"+");
+    S = S.replace(/ /g,"+");
+    var url = 'http://tinysong.com/b/' + A + "+" + S + '?format=json&key=f6834955de245c39810cf059ce77da5d';
+    http.get(url, function(res){
+        var data = '';
+        res.on('data', function (chunk){
+            data += chunk;
+        });
+        res.on('end', function(){
+            var obj = JSON.parse(data);
+            if(typeof obj["error"] != 'undefined'){
+                console.log('<======================ERROR WITH TINYSONG================================>');
+                console.log('<======================ERROR WITH TINYSONG================================>');
+                console.log('ERROR: ' + obj['error']);
+                console.log('<======================ERROR WITH TINYSONG================================>');
+                console.log('<======================ERROR WITH TINYSONG================================>');
+            }else{
+                ParseTinySong(obj);
+                console.log(obj);
+            }
+        });
+    });
+}
 
+function ParseTinySong (obj){
+    var SongID = obj["SongID"];
+    var SongName = obj["SongName"];
+    var ArtistName = obj["ArtistName"];
+    var AlbumName = obj["AlbumName"];
+//     console.log("SongID " + SongID + " SongName " + SongName + " ArtistName " + ArtistName + " AlbumName " + AlbumName)  
+    if(typeof SongID != 'undefined'){
+        var Songs = myFirebaseRef.child(SongID);
+        Songs.set({
+            ArtistName: ArtistName,
+            SongName: SongName,
+            AlbumName: AlbumName
+        });
+    }else{
+        console.log("Nothing here");
+    }
+}
 
 function splitTweet(TweetToSplit) {
     var arrayOfTweets = TweetToSplit.split("-");
@@ -20,9 +65,9 @@ function splitTweet(TweetToSplit) {
     var SongNm = SongStr.split("playing");
     var Song = SongNm[0].trim();
     var ArtistsNm = ArtistsStr.split("/");
-    console.log(ArtistsNm[0], Song);
+    TinySong(ArtistsNm[0], Song);
 }
-T.get('statuses/user_timeline', { screen_name: 'bpm_playlist', count: 1 }, function(err, data, response) {
+T.get('statuses/user_timeline', { screen_name: BPM, count: 1 }, function(err, data, response) {
     data.forEach(function(values){
         var tweet = values.text;
         splitTweet(tweet);
