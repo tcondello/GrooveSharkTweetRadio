a1f1ffdc13fc929a827681874d116c32037ab0ed
fpcond <fpcond@gmail.com>
2014-11-22 21:39:15 -0500

    directive work

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,15 @@
 /**
  * Created by Tim on 11/6/2014.
  */
-var groove = angular.module("groove", []);
-
-groove.controller("AppCtrl",['$scope', '$http', function($scope, $http){
-    $scope.results = [];
-
-//     $scope.search = function(Song){
-//         $scope.results.push($scope.Song);
-//     };
-
-    $scope.search = function() {
-        /* the $http service allows you to make arbitrary ajax requests.
-         * in this case you might also consider using angular-resource and setting up a
-         * User $resource. */
-        $http.get('/question', { params: Song },
-            function(response) { $scope.results = response; },
-            function(failure) { console.log("failed:" + failure);}
-        )};
-}]);
-
-groove.controller("ApiCtrl", ['$scope', '$resource', function($scope, $resource){
-    var TwitterAPI = $resource("http://search.twitter.com/search.json",
-        { callback: "JSON_CALLBACK"},
-        {get: { method: "JSONP" }});
-    $scope.search = function(){
-        $scope.searchResult = TwitterAPI.get({ q: $scope.searchTerm});
-    };
-
-}]);
-
-groove.controller("PostCtrl", function($scope, $http){
-    $http.get('http://ec2-54-90-254-168.compute-1.amazonaws.com/blotter/mark/CouncilDistrict.json').
-        success(function(data, status, headers, config) {
-            $scope.posts = data;
-        }).
-        error(function(data, status, headers, config) {
-            // log error
-        });
+var app = angular.module("sampleApp", ["firebase"]);
+app.controller("SampleCtrl", function($scope, $firebase) {
+    var ref = new Firebase("https://groovebmp.firebaseio.com/bpm_playlist");
+    var sync = $firebase(ref);
+    // create a synchronized array for use in our HTML code
+    $scope.bpmSong = sync.$asArray();
 });
+//app.directive('GroovePlayer', function() {
+//    return {
+//        template: '<div style="height: 400px; width:400px;">This is from directive</div>'
+//    };
+//});
