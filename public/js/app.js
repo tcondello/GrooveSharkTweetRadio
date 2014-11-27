07e711b9d5d487462b841d92280ebbfb9249a922
fpcond <fpcond@gmail.com>
2014-11-26 21:36:35 -0500

    directive work

--- public/js/app.js
+++ public/js/app.js
@@ -7,24 +7,13 @@ angular.module("GrooveApp", ["firebase"])
         var sync = $firebase(ref);
         // create a synchronized array for use in our HTML code
         $scope.bpmSong = sync.$asArray();
+        $scope.songID = $scope.bpmSong[0];
     })
     .directive('flashWidget', function(){
-
-        return {
-            restrict: 'E',
-            controller: 'MainCtrl',
-            scope: {
-                width:'=',
-                height:'=',
-                src: '=',
-                songID: '='
-            },
+        return{
+            restrict: 'EA',
             template: '<object width="250" height="40">'+
-            '<param name="movie" value="http://grooveshark.com/songWidget.swf">'+
-                '<param name="wmode" value="window">'+
-            '<param name="allowScriptAccess" value="always">'+
-                '<param name="flashvars"value="hostname=cowbell.grooveshark.com&amp;songIDs="{{songID}}"&amp;style=metal&amp;p=0">'+
-            '<embed src="http://grooveshark.com/songWidget.swf" type="application/x-shockwave-flash"width="250"height="40"flashvars="hostname=cowbell.grooveshark.com&amp;songIDs="{{songID}}"&amp;style=metal&amp;p=0" allowscriptaccess="always" wmode="window"></embed>'+
-                '</object>'
+            '<embed src="http://grooveshark.com/songWidget.swf" type="application/x-shockwave-flash" width="250" height="40" flashvars="hostname=cowbell.grooveshark.com&songIDs="{{}}"&style=metal&p=0" allowscriptaccess="always" wmode="window"></embed>'+
+            '</object>'
         }
     });
